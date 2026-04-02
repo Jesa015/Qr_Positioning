dbc1bdf0ea2611d05639f020f65a39ecf4b0b7b9
Jesa015 <boharayakendra482@gmail.com>
2026-04-02 15:16:08 +0300

    Add files via upload

--- 2504658.js
+++ 2504658.js
@@ -1,40 +1,40 @@
 const reader = new Html5Qrcode("camera");
 let scannerOn = false;
 
-function toggleScanner() {
+function toggleScanner(){
     scannerOn = !scannerOn;
     if (scannerOn) {
         startScanner();
         mapContainer.style.display = "none";
         btn.innerText = "CANCEL";
-    } else {
+    }else {
         stopScanner();
         mapContainer.style.display = "block";
         btn.innerText = "SCAN";
     }
 }
-
 function startScanner() {
     reader.start(
         { facingMode: "environment" },
         {},
         function (text) {
             const place = JSON.parse(text);
-
-            showMarkerAt(place.top, place.left);
-            showItemInfo(place); 
+            showMarkerAt(place.top, place.left, place.name, place.latitude, place.longitude);
             toggleScanner();
         }
-    ).catch(function (err) {
+    ).catch(function(err){
         console.error(err);
     });
 }
-
 function stopScanner() {
     reader.stop();
-}
-
-function showMarkerAt(top, left) {
+}  
+function showMarkerAt(top, left, name, inStock, price) {
     marker.style.top = top;
     marker.style.left = left;
+    document.getElementById("name").innerText = "Name: " + name;
+    document.getElementById("latitude").innerText = "latitude: " + latitude;
+    document.getElementById("longitude").innerText = "longitude: " + longitude;
+    
 }
+
